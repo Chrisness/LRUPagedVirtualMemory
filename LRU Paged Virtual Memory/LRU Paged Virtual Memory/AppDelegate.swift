c1f26ac8885cac939dd8aae90dbdff71cf821a25
Chrisness <cmahoney@gmail.com>
2015-04-06 02:49:52 -0400

    Starting the project! 🎉

---
+++ LRU Paged Virtual Memory/LRU Paged Virtual Memory/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LRU Paged Virtual Memory
+//
+//  Created by Chris Mahoney on 4/6/15.
+//  Copyright (c) 2015 Chris Mahoney. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LRU Paged Virtual Memory/LRU Paged Virtual Memory/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LRU Paged Virtual Memory
+//
+//  Created by Chris Mahoney on 4/6/15.
+//  Copyright (c) 2015 Chris Mahoney. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
